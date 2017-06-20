ed53cfff562ad4f2c2bf94aaccec22d219882658
zhangweiwei <yoyofly1@163.com>
2017-06-20 20:45:01 +0800

    test

--- test1.js
+++ test1.js
@@ -1,11 +1,16 @@
 var numLabel = document.querySelector(".numLabel");
+var fullLabel = document.querySelector(".totalLabel");
 // function logTest() {
 // 	console.log(test1);
 // 	alert("test1....");
 // }
 
-var curRestult;//当前计算的值
-var curValue;//当前计算器显示的值
+var curRestult = 0;//存贮当前计算的值
+var oldValueString;//存贮之前运算的字符串信息
+var curValueNum;//当前计算器显示的值，在= 号计算时，也是上一次加的值
+
+var changeType;//点击运算符后，记录运算符的类型  1:+, 2:-, 3:*, 4:/
+var isChange = false;//点运算符
 
 addEvent();
 //
@@ -30,12 +35,19 @@ function addEvent() {
 		switch (obj.id) {
 			case "0":
 				obj.addEventListener('pointerup', function () {
-					if (numLabel.textContent != '0') {
-						numLabel.textContent = this.id + numLabel.textContent;
-						curValue = parseFloat(numLabel.textContent);
+
+					if (isChange == true) {
+
+						isChange = false;
 					}
 					else {
-						curValue = 0;
+						if (numLabel.value != 0) {
+							numLabel.value = Number(numLabel.value + this.id);
+							curValueNum = numLabel.value;
+						}
+						else {
+							curValueString = 0;
+						}
 					}
 				})
 				break;
@@ -49,21 +61,111 @@ function addEvent() {
 			case "8":
 			case "9":
 				obj.addEventListener('pointerup', function () {
-					if (numLabel.textContent == '0') {
-						numLabel.textContent = this.id;
-						curValue = 0;
+					// if (numLabel.value == 0) {
+					// 	numLabel.value = Number(this.id);
+					// 	curValue = numLabel.value;
+					// }
+					// else {
+
+					if (isChange == true) {
+						numLabel.value = Number(this.id);
+						isChange = false;
 					}
 					else {
-						numLabel.textContent = this.id + numLabel.textContent;
-						urValue = parseFloat(numLabel.textContent);
+						numLabel.value = Number(numLabel.value + this.id);
+						curValueNum = numLabel.value;
 					}
+					console.log("isChange 222 == " + isChange);
 				})
 				break;
 			case "C":
 				obj.addEventListener('pointerup', function () {
-					numLabel.textContent = "0";
+					numLabel.value = 0;
+					fullLabel.textContent = "";
 					curRestult = 0;
-					curValue = 0;
+					curValueNum = 0;
+					oldValueString = undefined;
+				})
+				break;
+			case "+":
+				obj.addEventListener('pointerup', function () {
+					console.log(oldValueString);
+
+					//相同的类型的运算符，反复点击无效
+					if (isChange == true)
+					{
+						if(changeType == 1) {
+							return;
+						}
+						else if(changeType == 5)
+						{
+							curRestult = 0;
+						}
+					}
+					//存储文本内容
+					if (oldValueString != undefined) {
+						fullLabel.textContent = oldValueString + " + " + numLabel.value + " + ";
+					}
+					else {
+						fullLabel.textContent = numLabel.value + " + ";
+					}
+
+					oldValueString = fullLabel.textContent.slice(0, fullLabel.textContent.length - 3);
+
+					//计算已经存贮的值
+					numLabel.value = Number(numLabel.value) + Number(curRestult);
+					curRestult = numLabel.value;
+
+					//设置变量，存储开始运算
+					changeType = 1;
+					isChange = true;
+				})
+				break;
+			case "-":
+				obj.addEventListener('pointerup', function () {
+					numLabel.value = "0";
+					curRestult = 0;
+					curValueString = 0;
+				})
+				break;
+			case "*":
+				obj.addEventListener('pointerup', function () {
+					numLabel.value = "0";
+					curRestult = 0;
+					curValueString = 0;
+				})
+				break;
+			case "/":
+				obj.addEventListener('pointerup', function () {
+					numLabel.value = "0";
+					curRestult = 0;
+					curValueString = 0;
+				})
+				break;
+			case "=":
+				obj.addEventListener('pointerup', function () {
+
+					if (isChange == true && changeType == 5) {
+						//存储文本内容
+						fullLabel.textContent = "";
+						//计算已经存贮的值
+						numLabel.value = curValueNum + Number(curRestult);
+						curRestult = numLabel.value;
+					}
+					else {
+						//存储文本内容
+						fullLabel.textContent = "";
+
+						//计算已经存贮的值
+						curValueNum = Number(numLabel.value);//记录当前界面值，方便连续点击=号时计算用
+						numLabel.value = curValueNum + Number(curRestult);
+						curRestult = numLabel.value;
+					}
+
+					//设置变量，存储开始运算
+					changeType = 5;
+					isChange = true;
+					oldValueString = undefined;
 				})
 				break;
 			default:
